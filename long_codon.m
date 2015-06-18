ecca7294396b6edfd52217cb50a253b3cdbef3f4
dhildebrandt14 <dhildebrandt14@me.com>
2015-06-18 14:30:39 -0500

    Update

--- long_codon.m
+++ long_codon.m
@@ -15,11 +15,13 @@ Index1 = regexp(seqLong,codon1);
 Index2 = regexp(seqLong,codon2);
 Index3 = regexp(seqLong,codon3);
 
+Index1Length = length(Index1);
+
 fprintf('Index \n');    
 
 
 
-fprintf('%s OFFSETS: Index1(1) \n',codon1)
+fprintf('%s: Total Instances: %f \n     Offsets: %f %f %f %f %f \n             %f %f %f %f %f  \n',codon1,Index1Length,Index1(1),Index1(2),Index1(3),Index1(4),Index1(5),Index1(6),Index1(7),Index1(8),Index1(9),Index1(10))
 
 
 
