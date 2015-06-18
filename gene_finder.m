b930d0cac7cb26ca4902d4f26e8dacd98c8bf6e3
dhildebrandt14 <dhildebrandt14@me.com>
2015-06-18 18:16:31 -0500

    Update

---
+++ gene_finder.m
@@ -0,0 +1,48 @@
+%
+%
+clear;
+clc;
+
+fileID = fopen('long_sequence.txt','r');
+seqLong = fscanf(fileID,'%s');
+fclose(fileID);
+
+i = 1;
+sequenceLength = length(seqLong);
+
+startLocations = [];
+stopLocations = [];
+
+while i <( sequenceLength - 3)
+    
+    subSeq = seqLong(i:i+2);
+    
+    if (strcmp(subSeq,'TAC') == 1)
+        startLocations = [startLocations i];
+        i = i + 3;
+                if (strcmp(subSeq,'TAG') == 1)
+                stopLocations = [stopLocations i];
+                i = i + 3;
+         
+                elseif (strcmp(subSeq,'TAA') == 1)
+                stopLocations = [stopLocations i];
+                 i = i + 3;
+            
+                elseif (strcmp(subSeq,'TGA') == 1)
+                stopLocations = [stopLocations i];
+                 i = i + 3;
+        
+                else
+                     i = i + 3;
+            
+                end
+    
+    else i = i + 1;
+        
+    end
+    
+    
+        
+        
+    
+end
