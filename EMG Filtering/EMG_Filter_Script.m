a6e9d6ee6f66229606d149ad6bcec6f7811dd2d5
dhildebrandt14 <dhildebrandt14@me.com>
2015-06-21 00:57:47 -0500

    Updated to plot

--- EMG Filtering/EMG_Filter_Script.m
+++ EMG Filtering/EMG_Filter_Script.m
@@ -7,11 +7,11 @@ clc;
 
 filename = 'Force_EMG_Data.xls';
 timeRange = 'A1:A35000';
-forceRange = 'B1:B35000';
+%forceRange = 'B1:B35000';
 emgRange = 'C1:C35000';
 
 timeData = xlsread(filename,timeRange);
-forceData = xlsread(filename,forceRange);
+%forceData = xlsread(filename,forceRange);
 emgData = xlsread(filename,emgRange);
 
 rectifiedEMG = abs(emgData);
@@ -22,6 +22,8 @@ filterNumber = input('Enter filter value: ');
 
 filteredEMGData = [];
 i = 1;
+timeArray = [];
+timeCounter = 0;
 filterEnd = (i + filterNumber);
 endCheck = (emgDataLength - i);
 
@@ -30,12 +32,22 @@ while i < emgDataLength
         filterNumber = endCheck;
     end
     
-    filteredPoint = sum(rectifiedEMG(i:filterEnd))/filterNumber;
+    filteredPoint = (sum(rectifiedEMG(i:filterEnd))) / filterNumber;
     filteredEMGData = [filteredEMGData filteredPoint];
-    i = i + filterNumber;
+    i = i + 1;
+    timeCounter = timeCounter + 0.001;
+    timeArray = [timeArray timeCounter];
     
 end
-    
+
+figure 
+subplot(1,2,1)
+plot(timeArray,filteredEMGData)
+title('Filtered Data')
+
+subplot(1,2,2)
+plot(timeData,rectifiedEMG)
+title('Raw Data')
 
 
 
